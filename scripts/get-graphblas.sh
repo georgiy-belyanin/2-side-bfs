d18bb6eaf7a09c049798e4637d05f466bd2365c8
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-05-21 19:37:06 +0300

    Add exporting variables to get-graphblas script

--- scripts/get-graphblas.sh
+++ scripts/get-graphblas.sh
@@ -1,3 +1,5 @@
+export GRAPHBLAS_INCLUDE_DIR=`pwd`/graphblas-binaries/include
+export GRAPHBLAS_LIBRARY=`pwd`/graphblas-binaries/lib/libgraphblas.so
 mkdir graphblas-binaries
 cd graphblas-binaries
 wget --quiet https://anaconda.org/conda-forge/graphblas/7.4.1/download/linux-64/graphblas-7.4.1-hcb278e6_0.conda
