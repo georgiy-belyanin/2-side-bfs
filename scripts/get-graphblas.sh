f374ced7c9f3df670e26e2ebf4a6dd10ef1666e4
Georgiy Belyanin <33698739+georgiy-belyanin@users.noreply.github.com>
2024-05-21 16:46:07 +0300

    Add github actions CI

---
+++ scripts/get-graphblas.sh
@@ -0,0 +1,7 @@
+mkdir graphblas-binaries
+cd graphblas-binaries
+wget --quiet https://anaconda.org/conda-forge/graphblas/7.4.1/download/linux-64/graphblas-7.4.1-hcb278e6_0.conda
+unzip graphblas-7.4.1-hcb278e6_0.conda
+tar xf pkg-graphblas-7.4.1-hcb278e6_0.tar.zst
+cd ..
+
